47d65fa69f6fcb18e4d6d0ecc2f5efee5bb6695e
Ralphmore <raphealobairekanmi@gmail.com>
2026-04-09 00:49:30 +0100

    Block Vercel Indexing

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,22 @@
-import type { MetadataRoute } from "next"
+import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
+  const isVercel = process.env.VERCEL_URL?.includes('vercel.app')
+
+  if (isVercel) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/',
       },
-    ],
-    sitemap: "https://psalmsalive.com/sitemap.xml",
+    }
+  }
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://psalmsalive.com/sitemap.xml',
   }
 }
